37abffb253d2ea8f196ce94a0e2a5e51c88bc2b9
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-09-15 16:51:05 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carregando';
 
@@ -28,6 +29,11 @@ class Header extends React.Component {
     const { loading, user: { name } } = this.state;
     return (
       <header data-testid="header-component">
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </nav>
         { loading ? <Carregando />
           : <span data-testid="header-user-name">{ name }</span> }
       </header>
